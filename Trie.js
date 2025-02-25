a6ca24018d54b04b016c6240e5244a53d172c05c
AlexLe024 <a.gorlyshkov@gmail.com>
2025-02-25 18:19:08 +0500

    5/5

--- Trie.js
+++ Trie.js
@@ -1,8 +1,3 @@
-/**
- * Seminar 2.5 Simple Trie
- */
-
-
 class TrieNode {
     constructor(key) {
         this.key = key;
@@ -11,25 +6,51 @@ class TrieNode {
     }
 }
 
-
 class Trie {
     constructor() {
         this.root = new TrieNode(null);
     }
 
     insert(word) {
-        // TODO Insert word symbol by symbol
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const symbol = word[i];
+            if (!node.children[symbol]) {
+                node.children[symbol] = new TrieNode(symbol);
+            }
+            node = node.children[symbol];
 
+            if (i === word.length - 1) {
+                node.isWord = true;
+            }
+        }
     }
 
-    hasNode(word){
-        // TODO Check is word in Trie
-        return false;
+    hasNode(word) {
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const symbol = word[i];
+            if (!node.children[symbol]) {
+                return false; // Если символ отсутствует, слова нет
+            }
+            node = node.children[symbol];
+        }
+        return node.isWord; // Проверяем, является ли узел концом слова
     }
 
-    getAllNodes(){
-        // TODO returns all nodes as array
-        return [];
+    getAllNodes() {
+        const nodes = [];
+        const stack = [this.root]; // Используем стек для обхода в глубину (DFS)
+
+        while (stack.length > 0) {
+            const node = stack.pop();
+            nodes.push(node);
+            for (const child in node.children) {
+                stack.push(node.children[child]);
+            }
+        }
+
+        return nodes;
     }
 }
 
